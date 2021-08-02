4d4b401a9eb85c8f7285674ce80be20cdd97e3f8
Adil Wadhwania <71920126+AdilWadhwania@users.noreply.github.com>
2021-08-02 11:21:18 +0530

    Add files via upload

---
+++ NoteTaker/notes.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM hiber.notes
+LIMIT 0, 1000
+
+-- Date: 2021-08-02 11:13
+*/
+INSERT INTO `` (`id`,`addedDate`,`content`,`history`,`title`) VALUES (705,'2021-07-30 09:15:36','Science paper\r\n 1) section A(6 questions of one mark)\r\n 2)section B (9 questions each of 2 marks)\r\n 3) section C(6 questions each of 3 marks)\r\n 4) section D (5 questions each of 4 marks)','1','Exam paper');
+INSERT INTO `` (`id`,`addedDate`,`content`,`history`,`title`) VALUES (1913,'2021-07-30 09:41:38','Data science is used to collect data, manage and analyse the data.','1','What is Data Science ?');
+INSERT INTO `` (`id`,`addedDate`,`content`,`history`,`title`) VALUES (4574,'2021-07-30 08:48:54','Data science is used to collect data, manage and analyse the data.','0','Data Science ');
+INSERT INTO `` (`id`,`addedDate`,`content`,`history`,`title`) VALUES (9790,'2021-07-30 09:29:51','Python is an interpreted high-level general-purpose programming language. Python\'s design philosophy emphasizes code readability with its notable use of significant indentation. Its language constructs as well as its object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects. python is trending today ! ','1','what is python');
